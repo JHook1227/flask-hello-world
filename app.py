ab7cd76c508ec70a839c0c9513b88a7d4a2f75d7
JHook1227 <Jhook1173@gmail.com>
2025-11-09 20:36:02 -0700

    db create

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def testing():
     conn = psycopg2.connect("postgresql://joes_database_user:OZnyo4u2sOfojDFonBPWKhQ75gGrZYFt@dpg-d48jgvm3jp1c73ck6n6g-a/joes_database")
     conn.close()
     return "Database initiated successfully"
+
+@app.route('/db_create')
+def testers():
+    conn = psycopg2.connect("postgresql://joes_database_user:OZnyo4u2sOfojDFonBPWKhQ75gGrZYFt@dpg-d48jgvm3jp1c73ck6n6g-a/joes_database")
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+                );
+                ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfullly created"
