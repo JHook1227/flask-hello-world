c1d56fe5c75be40a2ff3850eafe28c360bcee523
JHook1227 <Jhook1173@gmail.com>
2025-11-09 19:17:27 -0700

    return 'Hello, World from Joe in 3308'
    @app.route('db_test')
    def testing():
        conn = psycopg2.connect("postgresql://joes_database_user:OZnyo4u2sOfojDFonBPWKhQ75gGrZYFt@dpg-d48jgvm3jp1c73ck6n6g-a/joes_database")
        conn.close()
        return "Database initiated successfully"
    ~added stuff                                                                                                                                                            ~                                                                                                                                                            .git/COMMIT_EDITMSG[+] [unix] (19:17 09/11/2025)

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Joe in 3308'
+
+@app.route('db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://joes_database_user:OZnyo4u2sOfojDFonBPWKhQ75gGrZYFt@dpg-d48jgvm3jp1c73ck6n6g-a/joes_database")
+    conn.close()
+    return "Database initiated successfully"
